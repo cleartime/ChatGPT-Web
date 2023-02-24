2a2fa3b176ec18fe32724759fa457aaa28813d2b
gxx <562606139@qq.com>
2023-02-24 18:09:02 +0800

    test2

--- src/views/chat/layout/sider/Footer.vue
+++ src/views/chat/layout/sider/Footer.vue
@@ -8,12 +8,5 @@ const show = ref(false)
 <template>
   <footer class="flex items-center justify-between min-w-0 p-4 overflow-hidden border-t">
     <UserAvatar />
-    <HoverButton tooltip="Setting" @click="show = true">
-      <span class="text-xl text-[#4f555e]">
-        <SvgIcon icon="ri:settings-4-line" />
-      </span>
-    </HoverButton>
-
-    <Setting v-model:visible="show" />
   </footer>
 </template>
