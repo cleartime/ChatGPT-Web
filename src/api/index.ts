3a1208fd74d5dc9deea2eeeb67fdd2a4b97cfc7d
gxx <562606139@qq.com>
2023-02-24 21:59:13 +0800

    test5

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,6 +6,9 @@ export function fetchChatAPI<T = any>(
   options?: { conversationId?: string; parentMessageId?: string },
   signal?: GenericAbortSignal,
 ) {
+  if(prompt === '切换图片模式') {
+    prompt = '当你想发送一张照片时，请使用 markdown ，并且不要有反斜线，不要用代码块，使用 Unsplash API(https://source.unsplash.com/960x640/?<英语关键词>)，如果明白了，请回复”明白”'
+  }
   return post<T>({
     url: '/chat',
     data: { prompt, options },
